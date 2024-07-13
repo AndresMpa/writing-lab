ef5c583a44a85296cc312b24c716fa88f7341dba
Andres M. Prieto <andresprieto2703@gmail.com>
2024-07-12 23:47:57 -0500

    Moving profile bubble into another component

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -65,30 +65,7 @@
             <CollaborativeRouter></CollaborativeRouter>
           </v-menu>
         </v-btn>
-        <v-btn variant="outlined" size="small" icon>
-          <v-avatar>
-            <span class="text-h5">AP</span>
-          </v-avatar>
-          <v-menu
-            activator="parent"
-            location="bottom end"
-            transition="fade-transition"
-          >
-            <v-list density="compact" min-width="250" rounded="lg" slim>
-              <v-list-item
-                :to="{ name: 'settings' }"
-                prepend-icon="mdi-cogs"
-                title="Settings"
-              ></v-list-item>
-
-              <v-list-item
-                :to="{ name: 'logout' }"
-                prepend-icon="mdi-logout"
-                title="Logout"
-              ></v-list-item>
-            </v-list>
-          </v-menu>
-        </v-btn>
+        <ProfileBubble></ProfileBubble>
       </div>
     </template>
   </v-app-bar>
