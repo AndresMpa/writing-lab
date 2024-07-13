655aced55214ba46529ca53d32482f5b823b427a
Andres M. Prieto <andresprieto2703@gmail.com>
2024-07-13 13:48:19 -0500

    Tracking avatar

---
+++ src/components/decoration/Avatar.vue
@@ -0,0 +1,34 @@
+<!-- GravatarImage.vue -->
+<template>
+  <v-avatar>
+    <span v-if="profileImage === ''" class="text-h5">
+      {{ username.charAt(0) }}
+    </span>
+
+    <v-img v-else :src="profileImage" :alt="username.charAt(0)" />
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    avatarSize: {
+      type: Number,
+      default: 80,
+    },
+  },
+  computed: {
+    profileImage() {
+      return "https://randomuser.me/api/portraits/women/80.jpg";
+    },
+    username() {
+      return "Jane Doe";
+    },
+    nickname() {
+      return "jDoe4";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
