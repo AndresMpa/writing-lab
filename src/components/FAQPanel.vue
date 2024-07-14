a498c638c5164281d5ae881c597d65c91f162c90
Andres M. Prieto <andresprieto2703@gmail.com>
2024-07-14 10:48:45 -0500

    Tracking FAQ component

---
+++ src/components/FAQPanel.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-expansion-panels class="my-4" variant="inset">
+      <v-expansion-panel
+        v-for="(item, index) in items"
+        :key="index"
+        :text="item.text"
+        :title="item.title"
+      ></v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: [String],
+      required: true
+    }
+  }
+};
+</script>
