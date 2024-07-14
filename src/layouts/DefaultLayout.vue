5badbd77fdb6c9dab7a04d5b7bdd657cef497e2d
Andres M. Prieto <andresprieto2703@gmail.com>
2024-07-14 12:31:34 -0500

    Tracking sites inside default bar

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -6,12 +6,22 @@
       <router-view />
     </v-main>
 
-    <AppFooter />
+    <AppFooter :sites="sites" />
   </v-app>
 </template>
 
 <script>
 export default {
   name: "DefaultLayout",
+  data: () => ({
+    sites: [
+      { icon: "mdi-instagram", href: "https://www.instagram.com/utpilex/" },
+      { icon: "mdi-web", href: "https://idiomas.utp.edu.co/" },
+      {
+        icon: "mdi-code-braces",
+        href: "https://github.com/AndresMpa/writing-lab",
+      },
+    ],
+  }),
 };
 </script>
