8b934450eb9478343bb2bb24fcdebc3192ead888
Andres M. Prieto <andresprieto2703@gmail.com>
2024-07-15 03:08:03 -0500

    Moving profile data into another component

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-banner stacked>
+    <template v-slot:prepend>
+      <Avatar :size="60"></Avatar>
+    </template>
+
+    <v-banner-text>
+      <h2 class="text-h6">{{ profile.username }}</h2>
+      <p>{{ profile.nickname }}</p>
+    </v-banner-text>
+
+    <v-divider vertical></v-divider>
+
+    <div v-for="course in profile.courses" class="ml-2">
+      <v-chip class="ml-1">{{ course }}</v-chip>
+    </div>
+  </v-banner>
+</template>
+
+<script>
+import Avatar from "@/components/decoration/Avatar.vue";
+
+export default {
+  props: {
+    profile: Object,
+  },
+};
+</script>
--- src/components/ProfileSettings.vue
+++ src/components/ProfileSettings.vue
@@ -4,22 +4,7 @@
 
     <v-card class="mx-auto" :loading="loading">
       <v-card-item>
-        <v-banner stacked>
-          <template v-slot:prepend>
-            <Avatar :size="60"></Avatar>
-          </template>
-
-          <v-banner-text>
-            <h2 class="text-h6">{{ username }}</h2>
-            <p>{{ nickname }}</p>
-          </v-banner-text>
-
-          <v-divider vertical></v-divider>
-
-          <div v-for="course in courses" class="ml-2">
-            <v-chip class="ml-1">{{ course }}</v-chip>
-          </div>
-        </v-banner>
+        <ProfileCard :profile="profileData"></ProfileCard>
       </v-card-item>
 
       <ProfileSettingsInfo></ProfileSettingsInfo>
@@ -42,31 +27,28 @@
 </template>
 
 <script>
-import Avatar from "@/components/decoration/Avatar.vue";
-
 export default {
   name: "SettingsProfile",
   data: () => ({
     loading: false,
   }),
   computed: {
-    username() {
-      return "Jane Doe";
-    },
-    nickname() {
-      return "jDoe4";
-    },
-    courses() {
-      return [
-        "Course 1",
-        "Course 2",
-        "Course 3",
-        "Course 4",
-        "Course 5",
-        "Course 6",
-        "Course 7",
-        "Course 8",
-      ];
+    profileData() {
+      const data = {
+        username: "Jane Doe",
+        nickname: "jDoe4",
+        courses: [
+          "Course 1",
+          "Course 2",
+          "Course 3",
+          "Course 4",
+          "Course 5",
+          "Course 6",
+          "Course 7",
+          "Course 8",
+        ],
+      };
+      return data;
     },
   },
 };
