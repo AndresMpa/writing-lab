3e8f65f652a183bd07fbfa94faa684647da94455
Andres M. Prieto <andresprieto2703@gmail.com>
2024-07-15 03:10:52 -0500

    Stats alternative
    Adding a component to perform better components for stats section

---
+++ src/components/decoration/Charts.vue
@@ -0,0 +1,40 @@
+<template>
+  <PolarArea :data="chartData" :options="chartOptions" aria-label="" />
+</template>
+
+<script>
+import { PolarArea } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "Chart",
+  components: { Bar },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
