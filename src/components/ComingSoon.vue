64cdf5bf15696d85dee1754dd7db748482e2281d
Andres Prieto <andresprieto2703@gmail.com>
2024-07-17 22:24:22 -0500

    Adding coming soon component

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-img
+          height="300"
+          max-width="500"
+          src="../assets/img/in-progresssvg.svg"
+          aspect-ratio="16/9"
+          class="mx-auto mt-12"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center">
+      <v-col cols="4">
+        <v-card>
+          <v-card-title class="text-center"> Coming soon </v-card-title>
+          <v-card-text class="text-center">
+            This section is a work in progress, keep in touch to explore it!
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
