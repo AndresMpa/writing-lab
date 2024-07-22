cae766229a9526918669b2139297dd1f475ced77
Andres Prieto <andresprieto2703@gmail.com>
2024-07-21 22:46:16 -0500

    Refactor
    Moving files into general usable components for other view

---
+++ src/components/CollaborativeBanner.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-window v-model="onboarding" show-arrows="hover">
+    <v-window-item v-for="(item, index) in top" :key="index">
+      <v-card
+        class="d-flex align-center justify-center py-12"
+        elevation="2"
+        height="200"
+      >
+        <div class="text-justify">
+          <v-chip size="x-small" color="primary">{{ item.level }}</v-chip>
+          <v-card-title class="title">{{ item.title }} </v-card-title>
+
+          <v-card-text class="w-75">
+            {{ item.description }}
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              v-if="item.id"
+              :to="{ name: 'entry', params: { id: item.id } }"
+              append-icon="mdi-link-variant"
+              class="ml-auto"
+              color="primary"
+              size="small"
+              text="Read"
+            />
+          </v-card-actions>
+        </div>
+      </v-card>
+    </v-window-item>
+  </v-window>
+</template>
+<script>
+export default {
+  props: {
+    top: {
+      type: [Object, null],
+      default: [
+        {
+          level: "Collaborative",
+          title: "Welcome to ILEX Writing Lab - Get Insights",
+          description:
+            "Here you will find different ideas to implement then in your classes",
+        },
+      ],
+    },
+  },
+  data: () => ({
+    onboarding: 0,
+  }),
+};
+</script>
+
+<style lang="sass" scoped>
+.title
+  white-space: normal
+</style>
---
+++ src/components/CollaborativeIterator.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-card variant="text" flat>
+    <v-data-iterator
+      :items-per-page="(columnsPerPost + 1) * rowsPerPost"
+      :loading="loading"
+      :search="search"
+      :items="post"
+    >
+      <template v-slot:header>
+        <v-toolbar class="px-2" flat>
+          <v-text-field
+            density="compact"
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            v-model="search"
+            hide-details
+            clearable
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:loader>
+        <v-container class="pa-1" fluid>
+          <v-row dense no-gutters>
+            <v-col
+              v-for="(_, index) in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]"
+              :md="columnsPerPost"
+              :key="index"
+              cols="auto"
+            >
+              <v-skeleton-loader
+                class="border ma-1"
+                type="image, article"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+
+      <template v-slot:default="{ items }">
+        <v-container class="pa-2" fluid>
+          <v-row dense>
+            <v-col
+              v-for="(item, key) in items"
+              :key="`${item.title}-${key}`"
+              :md="columnsPerPost"
+              cols="auto"
+            >
+              <CollaborativeIteratorItem
+                :img="noImg ? '' : item.raw.img"
+                :title="item.raw.title"
+                :description="noDescription ? '' : item.raw.description"
+                :level="item.raw.level"
+                :id="item.raw.id"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+
+      <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
+        <div class="d-flex align-center justify-center pa-4">
+          <v-btn
+            @click="prevPage"
+            :disabled="page === 1"
+            density="comfortable"
+            icon="mdi-arrow-left"
+            variant="tonal"
+            rounded
+          ></v-btn>
+
+          <div class="mx-2 text-caption">
+            Page {{ page }} of {{ pageCount }}
+          </div>
+
+          <v-btn
+            @click="nextPage"
+            :disabled="page >= pageCount"
+            density="comfortable"
+            icon="mdi-arrow-right"
+            variant="tonal"
+            rounded
+          ></v-btn>
+        </div>
+      </template>
+    </v-data-iterator>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    noImg: {
+      type: Boolean,
+      default: false,
+    },
+    noDescription: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    rowsPerPost: {
+      type: Number,
+      default: 2,
+    },
+    columnsPerPost: {
+      type: Number,
+      default: 3,
+    },
+    post: {
+      type: [Array],
+    },
+  },
+  data: () => ({
+    search: "",
+  }),
+};
+</script>
