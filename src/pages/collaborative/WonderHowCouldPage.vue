ebdc10bcaa433c70b197c875ceb16d4b23f1dff9
Andres Prieto <andresprieto2703@gmail.com>
2024-07-22 23:58:48 -0500

    Adapting forum view

--- src/pages/collaborative/WonderHowCouldPage.vue
+++ src/pages/collaborative/WonderHowCouldPage.vue
@@ -1,5 +1,26 @@
 <template>
- {{ this.router.path }}
+  <CollaborativeIterator
+    :post="postData"
+    :columnsPerPost="12"
+    noDescription
+    useDate
+    noImg
+  />
 </template>
 
-<script></script>
+<script>
+import { usePostStore } from "@/stores/postStore";
+
+const postStore = usePostStore();
+
+export default {
+  data: () => ({
+    postData: null,
+  }),
+  created() {
+    postStore.loadInsight();
+    this.postData = postStore.insightPost;
+    this.topData = postStore.insightPost;
+  },
+};
+</script>
