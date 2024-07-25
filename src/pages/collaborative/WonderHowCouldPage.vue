2fd2e4b8ac1c564e1ce92949e513fec8e8175a55
Andres Prieto <andresprieto2703@gmail.com>
2024-07-24 23:31:22 -0500

    Updating modules config

--- src/pages/collaborative/WonderHowCouldPage.vue
+++ src/pages/collaborative/WonderHowCouldPage.vue
@@ -1,11 +1,34 @@
 <template>
-  <CollaborativeIterator
-    :post="postData"
-    :columnsPerPost="12"
-    noDescription
-    useDate
-    noImg
-  />
+  <v-container fluid>
+    <v-row>
+      <v-col cols="8">
+        <CollaborativeIterator
+          :post="postData"
+          :itemsPerPage="6"
+          :columnsPerPost="12"
+          :rowsPerPost="6"
+          noDescription
+          useDate
+          noImg
+        />
+      </v-col>
+      <v-col>
+        <v-card flat>
+          <v-card-title class="d-flex justify-space-between mb-0 mt-2">
+            <h3>Last posted</h3>
+          </v-card-title>
+          <v-divider class="mt-2 mb-4"></v-divider>
+
+          <CollaborativeIteratorItem
+            :id="postData[0].id"
+            :title="postData[0].title"
+            :level="postData[0].level.name"
+            :date="postData[0].date"
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -16,11 +39,18 @@ const postStore = usePostStore();
 export default {
   data: () => ({
     postData: null,
+    statesToFilter: null,
+    states: ["Active", "Closed"],
   }),
   created() {
-    postStore.loadInsight();
-    this.postData = postStore.insightPost;
-    this.topData = postStore.insightPost;
+    postStore.loadWonder();
+    this.postData = postStore.wonderPost;
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.w-95 {
+  width: 95%;
+}
+</style>
--- src/pages/collaborative/WorkTogether.vue
+++ src/pages/collaborative/WorkTogether.vue
@@ -1,5 +1,43 @@
 <template>
- {{ this.router.path }}
+  <v-stepper alt-labels :items="['Peers', 'Course']" class="ma-12">
+    <template v-slot:item.1>
+      <ProfileList :peers="peers" @save-peers="savePeers" />
+    </template>
+
+    <template v-slot:item.2>
+      <ProfileSettingsCourses
+        @save-courses="saveCourses"
+        title="Course the post is focused on"
+        label=""
+      />
+    </template>
+  </v-stepper>
 </template>
 
-<script></script>
+<script>
+import { useTogetherStore } from "@/stores/togetherStore";
+
+const togetherStore = useTogetherStore();
+
+export default {
+  data: () => ({
+    peers: null,
+  }),
+  methods: {
+    savePeers(peers) {
+      const data = [...peers];
+      console.log(data);
+    },
+    saveCourses(courses) {
+      const data = [...courses];
+      console.log(data);
+
+      this.$router.push({ name: "draft" });
+    },
+  },
+  created() {
+    togetherStore.initStore();
+    this.peers = togetherStore.usersData;
+  },
+};
+</script>
