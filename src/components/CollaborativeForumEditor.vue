a56b030c5cbc0bde2478177b0febde55ba18df9b
Andres Prieto <andresprieto2703@gmail.com>
2024-07-26 04:18:55 -0500

    Tracking forum component

---
+++ src/components/CollaborativeForumEditor.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card flat>
+    <v-card-title class="d-flex justify-space-between mb-0 mt-2">
+      <h3>Write a new question</h3>
+    </v-card-title>
+
+    <v-divider class="mt-2 mb-4"></v-divider>
+
+    <v-textarea
+      v-model="title"
+      :rules="titleState"
+      placeholder="Question title"
+      class="mx-5 my-1"
+      variant="underlined"
+      autocomplete="on"
+      name="textarea"
+      rows="1"
+      auto-grown
+      counter
+    ></v-textarea>
+
+    <v-textarea
+      v-model="post"
+      :rules="descriptionState"
+      placeholder="Question description"
+      variant="underlined"
+      autocomplete="on"
+      name="textarea"
+      class="mx-5"
+      maxRows="5"
+      rows="5"
+      auto-grown
+      counter
+    ></v-textarea>
+
+    <v-divider class="mt-12 mb-4"></v-divider>
+
+    <v-card-actions class="d-flex">
+      <v-btn
+        class="ml-auto"
+        color="primary"
+        variant="tonal"
+        prepend-icon="mdi-email-newsletter"
+        @click="createPost"
+        >Ask</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "",
+    post: "",
+  }),
+
+  computed: {
+    titleState() {
+      return [this.title !== "" || "Every question needs a title"];
+    },
+    descriptionState() {
+      return [
+        (value) =>
+          value.length >= 25 || "Description must have at least 25 characters",
+        (value) =>
+          value.length <= 250 ||
+          "Description must have least than 250 characters",
+      ];
+    },
+  },
+
+  methods: {
+    createPost() {
+      console.log(this.title, this.post);
+    },
+  },
+};
+</script>
--- src/pages/collaborative/WonderHowCouldPage.vue
+++ src/pages/collaborative/WonderHowCouldPage.vue
@@ -4,7 +4,7 @@
       <v-col cols="8">
         <CollaborativeIterator
           :post="postData"
-          :itemsPerPage="6"
+          :itemsPerPage="5"
           :columnsPerPost="12"
           :rowsPerPost="6"
           noDescription
@@ -13,19 +13,7 @@
         />
       </v-col>
       <v-col>
-        <v-card flat>
-          <v-card-title class="d-flex justify-space-between mb-0 mt-2">
-            <h3>Last posted</h3>
-          </v-card-title>
-          <v-divider class="mt-2 mb-4"></v-divider>
-
-          <CollaborativeIteratorItem
-            :id="postData[0].id"
-            :title="postData[0].title"
-            :level="postData[0].level.name"
-            :date="postData[0].date"
-          />
-        </v-card>
+        <CollaborativeForumEditor />
       </v-col>
     </v-row>
   </v-container>
