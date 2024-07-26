f66470c9f738b714d316bb79d1dba575d48539ba
Andres Prieto <andresprieto2703@gmail.com>
2024-07-26 04:25:51 -0500

    Adding an component to track users as a list

---
+++ src/components/ProfileList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-card flat>
+      <v-card-title class="d-flex">
+        Collaborate with:
+        <v-chip v-for="user in collaborating" :key="user" class="mx-2">
+          {{ user }}
+        </v-chip>
+        <v-btn
+          @click="$emit('savePeers', selected)"
+          variant="tonal"
+          class="ml-auto"
+        >
+          Save
+        </v-btn>
+      </v-card-title>
+    </v-card>
+    <v-list lines="three">
+      <v-list-item v-for="(item, index) in peers" :key="index">
+        <v-container fluid>
+          <v-row>
+            <v-col cols="11">
+              <ProfileCard :profile="item" />
+            </v-col>
+            <v-col class="d-flex">
+              <v-checkbox-btn
+                class="d-flex justify-center align-center"
+                v-model="selected"
+                :value="item.id"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    peers: {
+      type: Array,
+    },
+  },
+  data: () => ({
+    selected: [],
+  }),
+  computed: {
+    collaborating() {
+      const filteredObjects = this.peers.filter((user) =>
+        this.selected.includes(user.id)
+      );
+      const usernames = filteredObjects.map((obj) => obj.username);
+      return usernames;
+    },
+  },
+};
+</script>
