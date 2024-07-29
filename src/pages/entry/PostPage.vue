ea75169e0aca37e65a7c58649940c03aeb62d0c7
Andres Prieto <andresprieto2703@gmail.com>
2024-07-29 18:06:18 -0500

    Tracking needs for edition

--- src/pages/entry/PostPage.vue
+++ src/pages/entry/PostPage.vue
@@ -3,12 +3,15 @@
     <div class="position-relative">
       <v-btn
         @click="logOut"
-        class="position-absolute top-0 left-0 ml-3 mt-3"
+        class="position-fixed top-0 left-0 ml-3 mt-3"
         prepend-icon="mdi-door-open"
         size="small"
       >
         Leave
       </v-btn>
+    </div>
+    <div v-if="enableActions" class="dial">
+      <CollaborativeDial :dialActions="dialData" />
     </div>
     <EntryView :item="postData" />
     <EntryAuthor :post="postData" :profile="authorData" />
@@ -20,25 +23,28 @@
 
 <script>
 import DisplayPicture from "@/components/decoration/DisplayPicture";
+import { useEditorStore } from "@/stores/editorStore";
+
 import { usePostStore } from "@/stores/postStore";
+import { useUserStore } from "@/stores/userStore";
 
 const postStore = usePostStore();
+const userStore = useUserStore();
+const editorStore = useEditorStore();
 
 export default {
   name: "PostPage",
   data: () => ({
     postData: null,
     authorData: null,
+    dialData: null,
   }),
-  created() {
-    this.$watch(() => this.$route.params.id, this.getData, {
-      immediate: true,
-    });
+  computed: {
+    enableActions() {
+      return this.authorData.some((user) => user.id === userStore.id);
+    },
   },
   methods: {
-    logOut() {
-      this.$router.back();
-    },
     async getData(id) {
       await postStore.getPostData(id);
       const rawData = postStore.postDetail;
@@ -46,6 +52,42 @@ export default {
       this.postData = rawData.postData;
       this.authorData = rawData.authorData;
     },
+    logOut() {
+      this.$router.back();
+    },
+    loadDataToEdit() {
+      editorStore.loadDataToEdit();
+    },
+    deletePost(id) {
+      postStore.deletePost(id);
+    },
+  },
+  created() {
+    this.$watch(() => this.$route.params.id, this.getData, {
+      immediate: true,
+    });
+
+    this.dialData = [
+      {
+        icon: "mdi-text-box-edit",
+        label: "Edit this post",
+        action: this.loadDataToEdit,
+      },
+      {
+        icon: "mdi-delete",
+        label: "Delete post",
+        action: this.deletePost,
+      },
+    ];
   },
 };
 </script>
+
+<style scoped>
+.dial {
+  position: fixed;
+  bottom: calc(100vh - 56px * 1);
+  left: calc(100vw - 56px * 1.8);
+  z-index: 100;
+}
+</style>
