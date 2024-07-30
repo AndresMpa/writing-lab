841771728d62d8e105709b30ba279dab9399b88f
Andres Prieto <andresprieto2703@gmail.com>
2024-07-30 16:41:35 -0500

    Tracking mock
    Adding a simple mock to display data at comments section

--- src/stores/forumStore.js
+++ src/stores/forumStore.js
@@ -4,6 +4,51 @@ import { insertPost } from "@/api/post.model";
 
 import { useUserStore } from "./userStore";
 
+const mockComments = () => {
+  return [
+    {
+      author: "Tsetman",
+      content:
+        "aoeuaoeuaoeueaoeuaoeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat consequuntur voluptatem repellendus possimus dolorem",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi.",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi.",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi.",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat consequuntur voluptatem repellendus possimus dolorem",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi. Lorem ipsum dolor, sit amet r, sit amet consectetuo asdfads  asdfasd alores vel quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi.",
+    },
+    {
+      author: "Tsetman",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid tenetur doloremque autem id modi atque. Fugit natus facilis, nostrum perferendis officia cumque similique ex. Quae eos in dolores vel quasi.",
+    },
+  ];
+};
+
 export const useForumStore = defineStore("forumStore", {
   state: () => ({
     postId: "new",
