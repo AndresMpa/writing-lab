a74390742b8aa40924d89dd2455c84ce81adf41e
Andres Prieto <andresprieto2703@gmail.com>
2024-08-02 18:51:55 -0500

    Removing hash needs

--- src/lib/hash.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const hashPassword = async (password) => {
-  const saltRounds = process.env.SALT_ROUNDS;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
-};
