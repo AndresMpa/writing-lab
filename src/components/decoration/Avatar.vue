030ae0f3d4029b0573963085e6a2130b534a8f42
Andres Prieto <andresprieto2703@gmail.com>
2024-08-08 17:44:35 -0500

    Quick fix

--- src/components/decoration/Avatar.vue
+++ src/components/decoration/Avatar.vue
@@ -1,11 +1,15 @@
 <!-- GravatarImage.vue -->
 <template>
   <v-avatar>
-    <span v-if="profile.image == ''" class="text-h5">
+    <v-img
+      v-if="profile.image"
+      :src="profile.image"
+      :alt="profile.nickname.charAt(0)"
+    />
+
+    <span v-else class="text-h5">
       {{ profile.nickname.charAt(0) }}
     </span>
-
-    <v-img v-else :src="profile.image" :alt="profile.nickname.charAt(0)" />
   </v-avatar>
 </template>
 
