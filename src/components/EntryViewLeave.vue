cf57368c7f0b0672cd9d625b1e87ea3f2fc54413
Andres M. Prieto <andresprieto2703@gmail.com>
2024-09-25 00:21:12 -0500

    Tracking new leave component

---
+++ src/components/EntryViewLeave.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="mx-auto">
+    <v-toolbar
+      :elevation="8"
+      :color="loading ? 'primary' : ''"
+      density="compact"
+      class="d-flex"
+    >
+      <v-btn prepend-icon="mdi-door-open" @click="leavePage">Leave</v-btn>
+      <v-spacer></v-spacer>
+      <h2 class="text-h5 mx-3">{{ title }}</h2>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: Boolean,
+    title: String,
+  },
+  methods: {
+    leavePage() {
+      this.$router.push({ name: "collaborate" });
+    },
+  },
+};
+</script>
