8e66125b12d36157060fa6eeeb991082d1ed59fa
Andres M. Prieto <andresprieto2703@gmail.com>
2024-10-02 23:15:58 -0500

    Adding loader for bucket

--- src/pages/academicLiteracy/AcademicLiteracyPage.vue
+++ src/pages/academicLiteracy/AcademicLiteracyPage.vue
@@ -1,6 +1,27 @@
 <template>
-  <AcademicIterator />
+  <div v-if="bucketFiles">
+    <AcademicIterator :bucketFiles="bucketFiles" />
+  </div>
+  <div v-else>
+    <DisplayPicture />
+  </div>
 </template>
 
 <script>
+import DisplayPicture from "@/components/decoration/DisplayPicture.vue";
+
+import { useBucketStore } from "@/stores/bucketStore";
+
+const bucketStore = useBucketStore();
+
+export default {
+  computed: {
+    bucketFiles() {
+      return bucketStore.bucketFiles;
+    },
+  },
+  created() {
+    bucketStore.list();
+  },
+};
 </script>
