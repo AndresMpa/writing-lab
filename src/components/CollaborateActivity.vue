088159fce186bef54a5e7ccbbb0eb1ea73a73494
Andres M. Prieto <andresprieto2703@gmail.com>
2024-10-04 17:48:32 -0500

    Updating iterator

--- src/components/CollaborateActivity.vue
+++ src/components/CollaborateActivity.vue
@@ -1,63 +1,89 @@
 <template>
-  <v-container class="pt-6" fluid>
-    <v-row v-for="(activity, index) in activities" class="mx-2">
-      <v-col class="py-4">
-        <h2 class="text-h4">
-          {{ activity.collaboration.title }}
-        </h2>
-        <div class="d-flex pa-3 justify-space-around">
-          <div
-            class="pa-2 my-auto activity-sizing"
-            v-for="(data, index) in activity.entries"
-            @click="sendTo(activity.to)"
-          >
-            <CollaborateIteratorItem
-              :id="data.postId"
-              :title="data.title"
-              :level="data.level"
-              :img="data.image"
-              :description="data.description"
-              :date="data.date"
-              :active="data.active"
-            />
+  <v-data-iterator
+    :items-per-page="itemsPerPage"
+    :loading="loading"
+    :search="search"
+    :items="post"
+    :page="page"
+  >
+    <template v-slot:default="{ items }">
+      <v-container class="pt-6" fluid>
+        <v-row v-for="(activity, index) in items" class="mx-2">
+          <v-col class="py-4">
+            <h2 class="text-h4">
+              {{ activity.raw.collaboration.title }}
+            </h2>
+            <div class="d-flex pa-3 justify-space-around">
+              <div
+                class="pa-2 my-auto activity-sizing"
+                v-for="(data, index) in activity.raw.entries"
+              >
+                <CollaborateIteratorItem
+                  :id="data.postId"
+                  :title="data.title"
+                  :level="data.level"
+                  :img="data.image"
+                  :description="data.description"
+                  :date="data.date"
+                  :active="data.active"
+                />
 
-            <v-alert
-              :icon="stages[index].icon"
-              :title="stages[index].title"
-              :color="stages[index].color"
-              class="my-4"
-            />
-          </div>
-        </div>
+                <v-alert
+                  :icon="stages[index].icon"
+                  :title="stages[index].title"
+                  :color="stages[index].color"
+                  class="my-4"
+                />
+              </div>
+            </div>
 
-        <div class="d-flex mt-3 mb-6">
-          <v-chip
-            v-for="level in activity.level"
-            class="pa-2 mr-auto text-truncate"
-            color="primary"
-          >
-            {{ level }}
-          </v-chip>
-          <v-chip class="pa-2 ml-auto text-truncate" color="secondary">
-            {{ activity.collaboration.ctype }}
-          </v-chip>
-        </div>
+            <div class="d-flex mt-3 mb-6">
+              <v-chip
+                v-for="level in activity.raw.level"
+                class="pa-2 mr-auto text-truncate"
+                color="primary"
+              >
+                {{ level }}
+              </v-chip>
+              <v-chip class="pa-2 ml-auto text-truncate" color="secondary">
+                {{ activity.raw.collaboration.ctype }}
+              </v-chip>
+            </div>
 
-        <v-divider class="mt-6"></v-divider>
-      </v-col>
-    </v-row>
-  </v-container>
+            <v-divider class="mt-6"></v-divider>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
+  </v-data-iterator>
 </template>
 
 <script>
 export default {
   props: {
-    activities: {
-      type: [Array, null],
-      required: true,
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    itemsPerPage: {
+      type: Number,
+      default: 5,
+    },
+    rowsPerPost: {
+      type: Number,
+      default: 2,
+    },
+    columnsPerPost: {
+      type: Number,
+      default: 3,
+    },
+    post: {
+      type: [Array],
     },
   },
   data: () => ({
+    search: "",
+    page: 1,
     stages: [
       {
         title: "Planning stage",
@@ -76,7 +102,15 @@ export default {
       },
     ],
   }),
+  computed: {
+    pageCount() {
+      return Math.ceil(this.post.length / this.itemsPerPage);
+    },
+  },
   methods: {
+    pageChange(page) {
+      this.$emit("pageChange", page);
+    },
     sendTo(path) {
       this.$router.push({ name: path });
     },
